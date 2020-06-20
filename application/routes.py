61c8b2ab65ffbc0199da0ce94e4e976dfd5bc22d
shivank-awasthi98 <shivankawasthi889@gmail.com>
2020-06-20 15:04:08 +0530

    section 4 done database added

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
-from application import app
+from application import app,db
 from flask import render_template,request ,json,Response
+from application.models import User,Course,Enrollment
 courseData=[{"courseID":"111","title":"PHP 111","description":"Intro to php","credits":"3","term":"Fall,Spring"},{"courseID":"111","title":"PHP 111","description":"Intro to php","credits":"3","term":"Fall,Spring"},{"courseID":"111","title":"PHP 111","description":"Intro to php","credits":"3","term":"Fall,Spring"},{"courseID":"111","title":"PHP 111","description":"Intro to php","credits":"3","term":"Fall,Spring"}]
 
 @app.route("/")
@@ -34,3 +35,24 @@ def api(idx=None):
     else:
         jdata=courseData[int(idx)]
     return Response(json.dumps(jdata),mimetype="application/json")
+
+
+
+
+
+@app.route("/user")
+def user():
+    # User(user_id  =  1,
+    #     first_name  =  'christian',
+    #     last_name  =  'Hur',
+    #     email  =  'christHur@uta.com',
+    #     password = 'aaa12@@@').save()
+    # User(user_id  =  2,
+    #     first_name  =  'christopher',
+    #     last_name  =  'Hur',
+    #     email  =  'christHur@uta.com',
+    #     password = 'aaa12@@@').save()
+    users=User.objects.all()
+    return render_template('user.html',users=users)
+    
+
